f425fa5e66926f26b1acbd265d684411c1a79c03
Alexandr1991Sh <an1991sh@yandex.ru>
2023-04-26 20:24:06 +0300

    lesson universal button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button} from "./Components/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    const clicks = (name: string, ade: number, address: string) => {
+        console.log(name, ade, address)
+    }
+    const click2 = (name: string) => {
+        console.log(name)
+    }
+
+
+    // const myFirstSubscribe = (event: MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Subscribe 1 Sasha')
+    // }
+    // const myTwoSubscribe = (event: MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Subscribe 2 Dima')
+    // }
+
+    // const onClickHandler = (name: string)=>{
+    //     console.log(name);
+    // }
+
+
+
+
+    return (
+        <>
+            {/*<button onClick={(event: MouseEvent<HTMLButtonElement>) => onClickHandler('Alex' )}>Subscribe 1</button>*/}
+            {/*<button onClick={(event: MouseEvent<HTMLButtonElement>) => onClickHandler('Alex')}>Subscribe 2</button>*/}
+            {/*<button onClick={() => onClickHandler('Dima')}>Subscribe 3</button>*/}
+
+
+            <Button callBack={() => clicks('Sasha', 32, 'city Moscow')} name={'Button number one'}/>
+            <Button callBack={() => clicks('Dima', 35, 'city Minsk')} name={"Button number two"}/>
+            <Button callBack={()=>click2('I am stupid button')} name={"Button number Three"}/>
+           
+        </>
+    )
 }
 
-export default App;
+export default App
+
+
+
